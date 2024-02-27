72f8e615e6dab18051dba3ced311d5c0ec23dc81
Roger Fontalvo <rogerjrfontalvo@gmail.com>
2024-02-27 08:22:00 -0500

    mismo codigo

---
+++ src/appnixas/AppNixas.java
@@ -0,0 +1,11 @@
+
+package appnixas;
+
+
+public class AppNixas {
+
+ 
+    public static void main(String[] args) {
+    }
+    
+}
--- src/modelo/dataBase.java
+++ src/modelo/dataBase.java
@@ -1,14 +1,34 @@
 
 package modelo;
 
+import java.sql.Connection;
+import java.sql.*;
 
 public class dataBase {
-    String url;
-    String driver;
+   private String url;
+   private String driver;
+   private Connection conexion;
     
     public dataBase(){
         this.driver = "jdbc:sqlite";
         this.url = "data.db";
-        
+      
+    }
+    
+    protected void conectar() throws SQLException{
+         this.conexion =  DriverManager.getConnection(this.url);
+        if (this.conexion.isClosed()){
+            System.out.println("Conectado");
+        }
+    }
+
+    protected void cerrar() throws SQLException{
+        if(!this.conexion.isClosed()){
+            this.conexion.close();
+        }
+    }
+    
+    protected Connection getConexion() {
+        return conexion;
     }
 }
